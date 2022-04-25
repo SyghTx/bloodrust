608ea684aed5a8f5d283c86dcf5ee55847128059
kxacku <52127059+kxacku@users.noreply.github.com>
2022-04-25 16:29:59 +0300

    Update and rename website.freekassa3.js to website.freekassa4.js

--- website.freekassa4.js
+++ website.freekassa4.js
@@ -571,7 +571,7 @@ curcontent["Oplata"] = {
 </form>\
 <form onsubmit="return freekassaHandler(event)" class="qiwi-inp-form" id="fk-inp-form" method="get" target="_blank" action="https://enot.io/pay">\
 <label for="fk-donation-amount" class="qiwi-inp-oplata" id="qiwi-inp-other">\
-	<div class="qiwi-widget-title">Enot.io<br><p>дополнительные способы пополнения</p></div>\
+	<div class="qiwi-widget-title">Enot.io<br><p>доп. способы пополнения</p></div>\
 	<div class="qiwi-inp-box">\
 			<label for="fk-donation-amount" class="qiwi-label">Cумма</label>\
 			<div class="qiwi-rub">₽</div>\
