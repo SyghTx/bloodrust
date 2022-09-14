8dcef635d55fa03dbb1da825b1c5133b8d835390
kxacku <52127059+kxacku@users.noreply.github.com>
2022-09-14 18:10:51 +0300

    Update and rename website.freekassa39.js to website.freekassa40.js

--- website.freekassa40.js
+++ website.freekassa40.js
@@ -555,7 +555,7 @@ curcontent["Oplata"] = {
 			<div class="qiwi-rub">₽</div>\
 			<input type="tel" placeholder="Сумма" id="qiwi-donation-amount" name="amount" required="" value="">\
 			<input type="hidden" id="qiwi-donation-comment" name="comment" value="Пожертвование на bloodrust.ru">\
-			<input type="hidden" name="publicKey" value="48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iP41L5FeJwQwwonvju9Y4nW8D1XD4UnFfdfn1ixQEW6PzikZpL1a3sXxvtNdufC4LfmqdGFDYUXymoe79eocUFFDPyEHxKwKPF28sYc7hXF">\
+			<input type="hidden" name="publicKey" value="48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iP41L5FeJwQwwonvju9Y4nW8D1XD4UnFfdfn1ixQEW6PzikZpL1a3sXxdhX2E92EpLgpKjQkxY9kzqDLX8JgZoxbdReH6dfmTDy7YsSDmkX">\
 			<input type="hidden" id="qiwi-donation-account" name="account" value="0">\
 			<input type="hidden" name="customFields[themeCode]" value="Danyl-Kzv6jl9NlE">\
 			<input type="hidden" id="qiwi-successUrl" name="successUrl" value="https://bloodrust.ru">\
